9628e399545b72a2a281bc0a32989978233712e5
Ageng Setiyawan <agengwan@icloud.com>
2025-04-12 23:56:36 +0800

    added state to josbs description to show more or less description

--- jobs-react/src/App.jsx
+++ jobs-react/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards'
 import JobListings from './components/JobListings'
+import ViewJobs from './components/ViewJobs'
 
 const App = () => {
   return (
@@ -10,13 +11,7 @@ const App = () => {
     <Hero />
     <HomeCards />
     <JobListings />
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
+    <ViewJobs />
     </>
   )
 }
