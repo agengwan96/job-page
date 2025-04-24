edba7c17ebbfad59779a0343d68f75714ad76f18
Ageng Setiyawan <agengwan@icloud.com>
2025-04-24 16:53:51 +0800

    added link to navbar.jsx

--- jobs-react/src/App.jsx
+++ jobs-react/src/App.jsx
@@ -1,11 +1,13 @@
 import {Router, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 import HomePage from './pages/HomePage'
-import MainLayout from './layouts/MainLayout';
+import MainLayout from './layouts/MainLayout'
+import JobsPage from './pages/JobsPage'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Router path='/' elements={<MainLayout/>}>
       <Router index elements={<HomePage/>}/>
+      <Router path='/jobs' elements={<JobsPage/>}/>
     </Router>
   )
 );
